85157b444840c73ee52e3093b96a4d9ba65c8f0d
Kieran R <kieranrutter2002@outlook.com>
2023-01-14 02:16:09 +0000

    Update DayExpanded.Designer.cs

--- Daily Digital Task Tracker/Daily Digital Task Tracker/DayExpanded.Designer.cs
+++ Daily Digital Task Tracker/Daily Digital Task Tracker/DayExpanded.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 100D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 100D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
             this.CreateEvent_btn = new System.Windows.Forms.Button();
             this.seconds_cmb = new System.Windows.Forms.ComboBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -98,7 +98,7 @@
             // 
             this.timer_lbl.AutoSize = true;
             this.timer_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timer_lbl.Location = new System.Drawing.Point(540, 22);
+            this.timer_lbl.Location = new System.Drawing.Point(552, 22);
             this.timer_lbl.Name = "timer_lbl";
             this.timer_lbl.Size = new System.Drawing.Size(143, 37);
             this.timer_lbl.TabIndex = 11;
@@ -139,24 +139,24 @@
             // 
             this.progressBar.BackColor = System.Drawing.Color.Transparent;
             this.progressBar.BorderlineColor = System.Drawing.Color.Transparent;
-            chartArea1.BackColor = System.Drawing.Color.Transparent;
-            chartArea1.BackSecondaryColor = System.Drawing.Color.Transparent;
-            chartArea1.BorderColor = System.Drawing.Color.Transparent;
-            chartArea1.Name = "ChartArea1";
-            chartArea1.ShadowColor = System.Drawing.Color.Transparent;
-            this.progressBar.ChartAreas.Add(chartArea1);
-            this.progressBar.Location = new System.Drawing.Point(455, 62);
+            chartArea2.BackColor = System.Drawing.Color.Transparent;
+            chartArea2.BackSecondaryColor = System.Drawing.Color.Transparent;
+            chartArea2.BorderColor = System.Drawing.Color.Transparent;
+            chartArea2.Name = "ChartArea1";
+            chartArea2.ShadowColor = System.Drawing.Color.Transparent;
+            this.progressBar.ChartAreas.Add(chartArea2);
+            this.progressBar.Location = new System.Drawing.Point(455, 45);
             this.progressBar.Name = "progressBar";
             this.progressBar.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
-            series1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series1.Name = "Series1";
-            series1.Points.Add(dataPoint1);
-            series1.Points.Add(dataPoint2);
-            series1.ShadowColor = System.Drawing.Color.Transparent;
-            this.progressBar.Series.Add(series1);
-            this.progressBar.Size = new System.Drawing.Size(333, 253);
+            series2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series2.Name = "Series1";
+            series2.Points.Add(dataPoint3);
+            series2.Points.Add(dataPoint4);
+            series2.ShadowColor = System.Drawing.Color.Transparent;
+            this.progressBar.Series.Add(series2);
+            this.progressBar.Size = new System.Drawing.Size(333, 270);
             this.progressBar.TabIndex = 16;
             this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
             // 
@@ -165,15 +165,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timer_lbl);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.date_lbl);
             this.Controls.Add(this.task_cmb);
             this.Controls.Add(this.hours_cmb);
-            this.Controls.Add(this.timer_lbl);
             this.Controls.Add(this.mins_cmb);
             this.Controls.Add(this.start_btn);
             this.Controls.Add(this.seconds_cmb);
             this.Controls.Add(this.CreateEvent_btn);
+            this.MinimumSize = new System.Drawing.Size(822, 506);
             this.Name = "DayExpanded";
             this.Text = "DayExpanded";
             this.Load += new System.EventHandler(this.DayExpanded_Load);
