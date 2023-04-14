ae88e233fbe12533a9f07c018a09243b7e96690a
kieran Rutter <kieranrutter2002@outlook.com>
2023-04-14 09:37:58 +0100

    Added the ability to take coins away.
    This will be used when the theme page is updated

--- Daily Digital Task Tracker/Daily Digital Task Tracker/coinControl.cs
+++ Daily Digital Task Tracker/Daily Digital Task Tracker/coinControl.cs
@@ -30,5 +30,18 @@ namespace Daily_Digital_Task_Tracker
             settings.writeini("SECTION", "coinCount", newCount);
             string coins = settings.coinCount;
         }
+
+        public static void takeCoins()
+        {
+            Settings settings = new Settings();
+            settings.readIni();
+
+            int coinCount = Int32.Parse(readCoins());
+
+            string newCount = (coinCount - 1).ToString();
+
+            settings.writeini("SECTION", "coinCount", newCount);
+            string coins = settings.coinCount;
+        }
     }
 }
